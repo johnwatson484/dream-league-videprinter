d462e23aabb913dad3171ec85737426b879599be
John Watson <john.watson1@defra.gov.uk>
2025-08-24 13:28:28 +0100

    Simplify index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import 'log-timestamp'
 import { createServer } from './server.js'
 import { startPoller } from './videprinter/poller/index.js'
+import config from './config.js'
 
 const init = async () => {
   const server = await createServer()
   await server.start()
   console.log('Server running on %s', server.info.uri)
-  const cfg = (await import('./config.js')).default
-  const ds = cfg.get('dataSource')
-  const vp = cfg.get('videprinter')
-  console.log(`[videprinter] using provider=${ds.provider} interval=${vp.pollLiveIntervalMs}ms`)
+  const ds = config.get('dataSource')
+  const vp = config.get('videprinter')
+  console.log(`[videprinter] provider=${ds.provider} useMock=${ds.useMock} pollLiveMs=${vp.pollLiveIntervalMs}`)
   startPoller(console)
 }
 
